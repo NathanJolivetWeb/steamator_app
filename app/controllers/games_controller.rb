c6d9fcf9fb2a4c493dd391aa9ef6dbc0364bf770
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-12-06 11:54:47 +0100

    setup cloudinary

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,20 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = game.new(game_params)
+    if @game.save
+      redirect_to games_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
 end
