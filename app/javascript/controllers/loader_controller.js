e231e48da8e5e442f8a454964d9fd2c2c46b0728
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-12-07 18:52:31 +0100

    add chartjs

--- app/javascript/controllers/loader_controller.js
+++ app/javascript/controllers/loader_controller.js
@@ -1,4 +1,5 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import Chart from 'chart.js/auto';
 
 export default class extends Controller {
   static values = {url: String}
@@ -10,17 +11,14 @@ export default class extends Controller {
     console.log('connected loader controller');
     console.log(this.urlValue);
 
-
     // fetch(this.urlValue)
     //   .then(blob => blob.json())
     //   .then(data => {
     //     console.log(data);
     //     console.log(data.owner);
     //     this.ownerTarget.insertAdjacentHTML('afterend', `<p>${data.owner}</p>`);
-
     //     this.predictionData = data
     //   });
-
 
     const blob = await fetch(this.urlValue)
     const data = await blob.json()
@@ -28,28 +26,45 @@ export default class extends Controller {
     // console.log(data.intercept);
     // console.log(data.slope);
     // console.log(data.owner);
-    this.ownerTarget.insertAdjacentHTML('afterend', `<p>${data.owner}</p>`);
+    // this.ownerTarget.insertAdjacentHTML('afterend', `<p>${data.owner}</p>`);
 
     this.predictionData = data
 
     const array = Array.from(Array(53).keys());
 
-
     const predictions_array = array.map((e) => {
-      return data.owner * (1 - 2 * (1 / (1 + Math.exp(data.intercept + 0.1 * e))));
+      return data.owner * (1 - 2 * (1 / (1 + Math.exp(0.2 + 0.1 * e))));
     });
-
+    predictions_array.unshift(0);
     console.log(data);
     console.log(array);
     console.log(predictions_array);
-
-
 
     // for (const num of [1,2,3,4]) {
     //   console.log(num)
     // }
 
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: array,
+            datasets: [{
+                label: 'Sales Prediction',
+                data: predictions_array,
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1,
+                borderWidth: 2,
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
   }
-
-
 }
