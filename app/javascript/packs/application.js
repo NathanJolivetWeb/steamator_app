050c8748f51dbb75ab1161e796843ed36d35d51e
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-12-07 23:35:51 +0100

    add some front

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,13 +24,13 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { textWrapperHomePage } from '../components/homepage';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  textWrapperHomePage();
 });
 
 import "controllers"
