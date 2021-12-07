ab9372ae67c9b424e337dc7690a0be4227e67992
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-12-07 09:38:30 +0100

    create user with avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 # Cleaning all DB model
 puts "Cleaning database..."
 Tag.destroy_all
+User.destroy_all
 puts "=============================="
+
+
+# Create users
+puts "Creating some users..."
+
+file = URI.open('https://avatars.githubusercontent.com/u/85315784?v=4')
+nathan = User.new(email: "nathan@lewagon.fr", password: "password")
+nathan.photo.attach(io: file, filename: 'avatar', content_type: 'image')
+nathan.save
 
 # Create tags
 puts "Creating some tags..."
