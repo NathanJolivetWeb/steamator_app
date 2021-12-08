81c824f976e7e94eb9f5f9ca8c4f1a7a22fe9ea2
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-12-08 12:38:24 +0100

    add validates

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,10 @@ class Game < ApplicationRecord
   has_one_attached :photo
   has_many :game_tags, dependent: :destroy
   has_many :tags, through: :game_tags
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :photo, presence: true
   def api_url
     tags = self.tags.map(&:name).to_json
     url = "https://steamator-deploy-api-2-se3v7w64ha-ew.a.run.app/?price=#{self.price}&tags=#{tags}&short_desc=#{self.description}"
